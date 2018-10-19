60330f0fc418c85fa78541b2ee7e56d6a0c1bc20
Kim <kmora2b@gmail.com>
2018-10-18 23:20:26 -0600

    Format
    Basic code

---
+++ scripts/myscript.js
@@ -0,0 +1,9 @@
+function hidePar() {
+	var par1 = document.getElementById("lastPar");
+	if (par1.style.display === "none") {
+		par1.style.display = "block";
+	} else {
+		par1.style.display = "none";
+	}
+	
+}
